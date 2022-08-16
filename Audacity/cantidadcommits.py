321c4414699c0971ac97172f601e91846621aed5
SoporteDeMovistar <109156054+SoporteDeMovistar@users.noreply.github.com>
2022-08-16 12:52:49 -0300

    Add files via upload

---
+++ Audacity/cantidadcommits.py
@@ -0,0 +1,11 @@
+str = open("log.txt","r").read()
+lista=str.split("commit")
+print(lista[:2])
+print(len(lista)-1)
+
+archivo_texto = open("cantidadcommits.txt", "w")
+
+for commit in lista:
+    archivo_texto.write(commit + "\n")
+
+archivo_texto.close()
